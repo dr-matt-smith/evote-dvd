e3ec23beb20933e5ffff1b467a48b1b656c0ecb8
dr-matt-smith <dr_matt_smith@me.com>
2016-02-24 22:59:19 +0000

    update SQL contents in /db

--- eVote_dvd_version22_specialSearchMethod/db/evote.sql
+++ eVote_dvd_version22_specialSearchMethod/db/evote.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.13.1
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:3306
--- Generation Time: Feb 23, 2016 at 06:48 AM
--- Server version: 10.0.20-MariaDB
--- PHP Version: 5.6.7
+-- Host: localhost:8889
+-- Generation Time: Feb 24, 2016 at 11:58 PM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `evote`
@@ -26,25 +20,47 @@ SET time_zone = "+00:00";
 -- Table structure for table `dvds`
 --
 
-CREATE TABLE IF NOT EXISTS `dvds` (
+CREATE TABLE `dvds` (
   `id` int(11) NOT NULL,
-  `title` text NOT NULL,
-  `category` text NOT NULL,
-  `price` float NOT NULL,
-  `voteAverage` int(11) NOT NULL,
+  `title` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `voteAverage` decimal(10,0) NOT NULL,
   `numVotes` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `dvds`
 --
 
 INSERT INTO `dvds` (`id`, `title`, `category`, `price`, `voteAverage`, `numVotes`) VALUES
-(1, 'Jaws', 'thriller', 10, 5, 1),
-(2, 'Jaws II', 'thriller', 5.99, 90, 77),
-(3, 'Shrek', 'comedy', 10, 50, 5),
-(4, 'Shrek II', 'comedy', 4.99, 0, 0),
-(5, 'Alien', 'scifi', 19.99, 95, 201);
+(1, 'Jaws', 'thriller', '10', '5', 1),
+(2, 'Jaws II', 'thriller', '6', '90', 77),
+(3, 'Shrek', 'comedy', '10', '50', 5),
+(4, 'Shrek II', 'comedy', '5', '0', 0),
+(5, 'Alien', 'scifi', '19', '95', 201);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `user` text NOT NULL,
+  `timestamp` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `text`, `user`, `timestamp`) VALUES
+(41, 'hello world', 'someone', 1456354649),
+(42, 'luke - I am your father ...', 'darth99', 1456354666),
+(43, 'a spoonful of suger helps the medicine go down', 'mary p.', 1456354682);
 
 --
 -- Indexes for dumped tables
@@ -57,6 +73,12 @@ ALTER TABLE `dvds`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +87,8 @@ ALTER TABLE `dvds`
 --
 ALTER TABLE `dvds`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=44;
